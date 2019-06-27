228cc75a4b3137e78711ba885a91255f3d91229f
onlymash <im@fiepi.me>
2019-06-27 11:27:26 +0800

    Remove donation info

--- app/src/main/java/onlymash/flexbooru/ap/ui/fragment/AboutFragment.kt
+++ app/src/main/java/onlymash/flexbooru/ap/ui/fragment/AboutFragment.kt
@@ -5,10 +5,8 @@ import android.os.Bundle
 import androidx.core.net.toUri
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
-import com.google.android.material.snackbar.Snackbar
 import onlymash.flexbooru.ap.BuildConfig
 import onlymash.flexbooru.ap.R
-import onlymash.flexbooru.ap.extension.copyText
 import onlymash.flexbooru.ap.extension.launchUrl
 import onlymash.flexbooru.ap.extension.openAppInMarket
 import onlymash.flexbooru.ap.ui.CopyrightActivity
@@ -44,19 +42,6 @@ class AboutFragment : PreferenceFragmentCompat() {
                     action = Intent.ACTION_SENDTO
                     data = email.toUri()
                 }, getString(R.string.share_via)))
-            }
-            "about_donation_paypal" -> {
-                context?.launchUrl("https://www.paypal.me/fiepi")
-            }
-            "about_donation_alipay" -> {
-                val text = "im@fiepi.com"
-                context?.copyText(text)
-                view?.let { Snackbar.make(it, getString(R.string.placeholder_copy_text, text), Snackbar.LENGTH_LONG).show() }
-            }
-            "about_donation_btc" -> {
-                val text = "bc1qxanfk3hc853787a9ctm28x9ff0pvcyy6vpmgpz"
-                context?.copyText(text)
-                view?.let { Snackbar.make(it, getString(R.string.placeholder_copy_text, text), Snackbar.LENGTH_LONG).show() }
             }
             "about_app_rate" -> {
                 context?.run {
